e29067b3a77e1ec7df85c60a0b20d20292f7def5
Vivesh Yadav <viveshofficial@gmail.com>
2022-02-11 11:58:48 +0530

    Add TextInput concept

--- App.js
+++ App.js
@@ -1,23 +1,56 @@
-import { StatusBar } from 'expo-status-bar'
 import React, {useState} from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   const [name, setName] = useState('Jerry Maguire')
+  const [age, setAge]= useState('31')
+  const [greeting, setGreeting]= useState('Namaste!')
 
-  const updateName = ()=>{
-    setName('Vivesh')
+  const updateName = (name)=>{
+    setName(name)
   }
+
+  const updateAge = (val)=>{
+    setAge(val)
+
+  }
+
+  const updateGreeting = ()=>{
+    setGreeting('Hare Krishna!')
+  }
+
   return (
     <View style={styles.container}>
-    
-      <View style={styles.body}>
-        <Text>My name is {name}</Text>
+      
+      <Text>{greeting}</Text>
+      
+      <View style={styles.container}> 
+        <Text>Bio:</Text>
+
+        <Text>Name is: {name}</Text>
+        <TextInput 
+          placeholder='Enter name'
+          onChangeText={(val)=> updateName(val)}
+          style={styles.input}
+        />
+
+        <Text>Age is: {age}</Text>
+        <TextInput 
+          keyboardType='numeric'
+          placeholder='Enter age'
+          onChangeText={(val)=> updateAge(val)}
+          style={styles.input}
+        />
+
       </View>
       
+
       <View style={styles.buttonContainer}>
-        <Button title='update name' onPress={updateName}/>
+        <Button title='update greeting' onPress={updateGreeting}/>
       </View>
+
+
+     
 
     </View>
   );
@@ -33,5 +66,13 @@ const styles = StyleSheet.create({
 
   buttonContainer:{
     marginTop:20
+  },
+
+  input:{
+    borderWidth:1,
+    borderColor:'#777',
+    margin:10,
+    width:200,
+    padding:8
   }
 });
