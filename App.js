9c3f9e35950566f5e9b235b291e96a85d1870615
Vivesh Yadav <viveshofficial@gmail.com>
2022-02-11 09:54:36 +0530

    Add text, view and stylesheet concept

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.boldText}> You had me at hello</Text>
+      </View>
+      <View style={styles.body}>
+        <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </Text>
+        <Text style={styles.boldText}> Lorem ipsum dolor <Text>Inherit</Text> sit amet, consectetur adipiscing elit. </Text>
+        <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </Text>
+        <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +27,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header:{
+    backgroundColor:'pink',
+    padding:20
+  },
+  boldText:{
+    fontWeight:'bold'
+  },
+  body:{
+    backgroundColor:'yellow',
+    padding:20,
+    fontWeight: 'bold'
+  }
 });
