c3169e0f1399fc6bb75d3595d4836cba36f1f8dc
Vivesh Yadav <viveshofficial@gmail.com>
2022-04-03 19:08:29 +0530

    Add modal structure

--- screens/Home.js
+++ screens/Home.js
@@ -1,9 +1,12 @@
-import { StyleSheet, Text, View, Button, FlatList, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, FlatList, TouchableOpacity, Modal } from 'react-native'
+import React, { useState } from 'react'
 import { globalStyles } from '../styles/GlobalStyles'
 import Card from '../shared/Card'
+import {MaterialIcons} from '@expo/vector-icons'
 
 const Home = ({navigation}) => {
+
+  const [modalVisible, setModalVisible] = useState(false)
 
   const reviews = [
     { title:'Peaky Blinders', body:'lorem ipsum', rating:4, key:1},
@@ -15,9 +18,31 @@ const Home = ({navigation}) => {
       item
     )
   }
+
   return (
     <View>
-      <Text style={globalStyles.titleText}>Home</Text>
+      <Modal 
+        visible={modalVisible}
+        animationType='slide'
+      >
+        <View style={StyleSheet.modalContent}>
+            <MaterialIcons 
+              name='close' 
+              size={24} 
+              onPress={()=>setModalVisible(false)}
+              style={[styles.modalToggle, styles.modalClose]}
+            />
+          <Text>
+            Hello from the modal
+          </Text>
+        </View>
+      </Modal>
+      <MaterialIcons 
+        name='add' 
+        size={24} 
+        onPress={()=>setModalVisible(true)}
+        style={styles.modalToggle}
+      />
         <FlatList 
           data={reviews}
           renderItem={({item})=>(
@@ -35,3 +60,18 @@ const Home = ({navigation}) => {
 }
 
 export default Home
+
+const styles = StyleSheet.create({
+  modalToggle:{
+    marginBottom:10,
+    borderWidth:4,
+    borderColor:'#f2f2f2',
+    padding:10,
+    borderRadius:10,
+    alignSelf:'center'
+  },
+  modalClose:{
+    marginTop:20,
+    marginBottom:0
+  }
+})
